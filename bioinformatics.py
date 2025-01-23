1b05bff69710306281be770cc51d9ddb03fdbbbc
jordynn20 <67450018+jordynn20@users.noreply.github.com>
2025-01-23 10:26:47 -0800

    Update bioinformatics.py

--- bioinformatics.py
+++ bioinformatics.py
@@ -1,1 +1,10 @@
+#code to identify the GC content of a string (decimal)
+string.count('G' and 'C')/len(string)
+
+#code to identify the GC content of a string (percent)
+(string.count('G' and 'C')/len(string))*100
+
+#function for this
+def GC_content(string): 
+  return string.count('G' and 'C')/len(string)
 
